bb824c1e89778d431139cfb62e01e941751fd43a
Melbin Mathew <mmathew@trl.co.uk>
2022-01-07 18:48:13 +0530

    Initial Commit

---
+++ war-game/war_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_gameApp.swift
+//  war-game
+//
+//  Created by Melbin Mathew on 07/01/22.
+//
+
+import SwiftUI
+
+@main
+struct war_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
